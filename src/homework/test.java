f39af89b254036dcde289676a191268f13f2002c
Yermek <yermek.sultangaleyev@gmail.com>
2022-10-07 01:22:54 -0400

    Hello

---
+++ src/homework/test.java
@@ -0,0 +1,31 @@
+package homework;
+
+public class test {
+
+//    public static void main(String[] args) {
+//
+//        int a = 9;
+//        int b = 7;
+//
+//        if (a >= b) {
+//            System.out.println("Pass");
+//        } else {
+//            System.out.println("Fail");
+//        }
+//    }
+
+    public static void main(String[] args) {
+        double aa = 3;
+        double bb = 5;
+        double cc = 9;
+        double sum = (aa * bb - cc);
+
+        if (sum > 0){
+
+        System.out.println(sum);
+        } else {
+        System.out.println(0);
+        }
+
+    }
+}
